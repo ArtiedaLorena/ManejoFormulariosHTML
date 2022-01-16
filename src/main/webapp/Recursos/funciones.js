11703dfa6e734eb602fb8e6b3f62b0839d3fad61
Lorena Artieda <76899464+ArtiedaLorena@users.noreply.github.com>
2022-01-16 20:35:54 -0300

    Add files via upload

---
+++ src/main/webapp/Recursos/funciones.js
@@ -0,0 +1,59 @@
+function validarForma(forma){
+    var usuario= forma.usuario;
+    if(usuario.values ==""|| usuario.value=="Escribir usuario"){
+        alert("Debe proporcionar un nombre de usuario");
+        usuario.focus();
+        usuario.select();
+        return false;
+        
+    }
+    
+    var password= forma.password;
+    if(password.values==""|| password.values.lenght<3){
+        alert("Debe proporcionar un password almenos de 3 caracteres");
+        password.focus();
+        password.select();
+        return false;
+    }
+    
+    var tecnologias =forma.tecnologia;
+    var checkSeleccionado=false;
+    
+    for(var i=0;i< tecnologias.lenght;i++){
+        if (tecnologias[i].checked){
+            checkSeleccionado = true;
+        }
+        
+    }   
+    
+    if(!checkSeleccionado){
+        alert("Debe seleccionar una tecnologia");
+                return false;
+    }
+    
+    var generos = forma.genero;
+    var radioSeleccionado = false;
+    
+    for (var i=0; i<generos.lenght;i++){
+        if (generos[i].checked){
+            radioSeleccionado=true;
+            
+        }
+    }
+    
+    if(!radioSeleccionado){
+        alert("Debe seleccionar un genero");
+        return false;
+    }
+    
+    var ocupacion =forma.ocupacion;
+    
+    if(ocupacion.value == ""){
+        alert("Debe seleccionar una ocupacion");
+        return false;
+    }
+    
+    //Formulario es valido
+    alert("Formulario valido, enviando datos al servidor")
+    return true;
+}
